69e31e1ff67d3427d93f10e654f518b7c05c2d4e
santiago <santiagoshinder@gmail.com>
2022-09-12 20:12:15 -0300

    Continue with the progress of the angular seminar

---
+++ src/app/beer-list/Beer.ts
@@ -0,0 +1,8 @@
+export  interface Beer {
+    name: string;
+    type: string;
+    price: number;
+    stock: number;
+    image: string;
+    clearence: boolean;
+}
--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Beer } from './beer';
 
 @Component({
   selector: 'app-beer-list',
@@ -6,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beer-list.component.scss']
 })
 export class BeerListComponent implements OnInit {
+    beers: Beer [] = [
+    {
+      name: "Amstel",
+      type: "Lager",
+      price: 450,
+      stock: 0,
+      image: "assets/img/amstel.jpg",
+      clearence: false
+    },
+    {
+      name: "Fuller's",
+      type: "Porter",
+      price: 550,
+      stock: 1000,
+      image: "assets/img/fuller.jpg",
+      clearence: true
+    },
+    {
+      name: "Hobgoblin",
+      type: "Indian Pale Ale",
+      price: 650,
+      stock: 1200,
+      image: "assets/img/hobgoblin.jpg",
+      clearence: false
+    },
+    {
+      name: "Kilkenny IRISH RED ALE ",
+      type: "Irish Red Ale",
+      price: 550,
+      stock: 700,
+      image: "assets/img/kilkenny.jpg",
+      clearence: false
+    }
+  ];
+
 
   constructor() { }
 
