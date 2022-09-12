ffc63014a06fd4454a6d56fb52d2f4d1f37a642c
santiago <santiagoshinder@gmail.com>
2022-09-12 01:53:01 -0300

    Created a repository with first angular proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerListComponent
   ],
   imports: [
     BrowserModule,
